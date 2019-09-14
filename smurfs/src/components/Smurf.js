713c10d6837b7b36668fd4a15334f0090b6f0eec
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-09-14 15:54:50 -0400

    Refactored Smurf.js to be a functional component.

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,37 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Smurf.css";
 import { connect } from "react-redux";
 import { deleteSmurf } from "../actions";
 
-class Smurf extends Component {
-  deleteSmurf = (e, id) => {
+const Smurf = ({ deleteSmurf, smurf }) => {
+  const callDeleteSmurf = (e, id) => {
     e.preventDefault();
-    this.props.deleteSmurf(id);
+    deleteSmurf(id);
   };
 
-  render() {
-    return (
-      <div className="Smurf">
-        <header>
-          <h2>{this.props.smurf.name}</h2>
-          <button onClick={e => this.deleteSmurf(e, this.props.smurf.id)}>
-            X
-          </button>
-        </header>
+  return (
+    <div className="Smurf">
+      <header>
+        <h2>{smurf.name}</h2>
+        <button onClick={e => callDeleteSmurf(e, smurf.id)}>X</button>
+      </header>
 
-        <div className="height">
-          <h3>height:</h3>
-          <p>{this.props.smurf.height} tall</p>
-        </div>
-
-        <div className="age">
-          <h3>Age:</h3>
-          <p>{this.props.smurf.age} smurf years old</p>
-        </div>
+      <div className="height">
+        <h3>height:</h3>
+        <p>{smurf.height} tall</p>
       </div>
-    );
-  }
-}
+
+      <div className="age">
+        <h3>Age:</h3>
+        <p>{smurf.age} smurf years old</p>
+      </div>
+    </div>
+  );
+};
 
 export default connect(
   null,
